f31a750f6d14d335cf41da4dd8c56b4e599ce545
Jarod Brennfleck <robinjars@gmail.com>
2020-09-22 09:12:39 +1000

    template created

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Going to use express to serve up a simple app that renders using Scetch.");
+/* 
+ * The purpose of this file is to act as the entry point for the application.
+ * 
+ * Author: Jarod Brennfleck
+ * 21 Sep 20
+ */
+
+console.log("Going to use express (maybe MetorJS) to serve up a simple app that renders using Scetch.");
+console.log("Deploy to Heroku: https://devcenter.heroku.com/articles/getting-started-with-nodejs#prepare-the-app");
