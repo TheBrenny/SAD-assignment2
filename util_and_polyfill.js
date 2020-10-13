ea898b0e898dced6c9ef5689aba5d96107623a4a
Jarod Brennfleck <iam+dev@justbrenny.me>
2020-10-13 14:26:58 +1100

    added replaceAll and matchAll

--- util_and_polyfill.js
+++ util_and_polyfill.js
@@ -4,3 +4,20 @@ Array.prototype.hasCombination = function (a2) {
 Object.isNull = function (...args) {
     return args.filter(e => e === undefined || e === null).length != 0;
 };
+String.prototype.replaceAll = function (find, replace) {
+    if (find.constructor.name === "String") {
+        find = new RegExp(find, "g");
+    }
+    if (find.constructor.name !== "RegExp") return this;
+    if (!find.global) find = new RegExp(find, find.flags + "g");
+    return this.replace(find, replace);
+};
+// Matchall polyfill - this is how we handle Node <12
+String.prototype.matchAll = function (rx) {
+    if (typeof rx === "string") rx = new RegExp(rx, "g");
+    rx = new RegExp(rx);
+    let cap = [];
+    let all = [];
+    while ((cap = rx.exec(this)) !== null) all.push(cap);
+    return all;
+};
